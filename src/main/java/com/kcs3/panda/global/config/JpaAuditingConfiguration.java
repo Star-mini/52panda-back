aa69b8eba1ea6b257839eb51d66afd1030e8e7b3
kjh <akkaa1119@naver.com>
2024-04-23 10:05:55 +0900

    feat: BaseEntity time 기본설정

---
+++ src/main/java/com/kcs3/panda/global/config/JpaAuditingConfiguration.java
@@ -0,0 +1,10 @@
+package com.kcs3.panda.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+
+}
