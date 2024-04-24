f2d0cb8a707c1e0cbbf4a16c31c1326f22f2dc84
kjh <akkaa1119@naver.com>
2024-04-24 11:09:21 +0900

    feat: 채팅기록 조회 기능 서비스단 추가

--- src/main/java/com/kcs3/panda/domain/chat/service/ChattingService.java
+++ src/main/java/com/kcs3/panda/domain/chat/service/ChattingService.java
@@ -1,10 +1,16 @@
 package com.kcs3.panda.domain.chat.service;
 
 import com.kcs3.panda.domain.chat.dto.ChatMessageDto;
+import com.kcs3.panda.domain.chat.dto.ChatRoomDto;
 import com.kcs3.panda.domain.chat.entity.ChattingContent;
 import com.kcs3.panda.domain.chat.entity.ChattingRoom;
 import com.kcs3.panda.domain.chat.repository.ChattingContentRepository;
 import com.kcs3.panda.domain.chat.repository.ChattingRoomRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,14 +28,38 @@ public class ChattingService {
         Long chatUserId = chatMessageDto.getChatUser();
         String content = chatMessageDto.getContent();
 
+        ChattingRoom room = chattingRoomRepository.findById(roomId)
+                .orElseThrow(() -> new EntityNotFoundException("ChattingRoom not found with id: " + roomId));
+
         log.info("사용자"+chatUserId+":"+content);
         ChattingContent chattingContent = ChattingContent.builder()
-                .chattingRoom(chattingRoomRepository.findById(roomId).get())
+                .chattingRoom(room)
                 .chatUserId(chatUserId)
                 .chatContent(content)
                 .build();
 
         chattingContentRepository.save(chattingContent);
 
+    }
+
+    public List<ChatRoomDto> getAllChatRooms(Long userId){
+        List<ChattingRoom> chattingRooms = chattingRoomRepository.findByBuyerIdOrSellerId(userId,userId);
+
+        return chattingRooms.stream()
+                .map(chattingRoom -> ChatRoomDto.builder()
+                        .id(chattingRoom.getId())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    public List<ChatMessageDto> getChatRoomContents(Long roomId){
+        List<ChattingContent> chattingContents = chattingContentRepository.findByChattingRoomId(roomId);
+
+        return chattingContents.stream()
+                .map(chattingContent -> ChatMessageDto.builder()
+                        .chatUser(chattingContent.getChatUserId())
+                        .content(chattingContent.getChatContent())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
