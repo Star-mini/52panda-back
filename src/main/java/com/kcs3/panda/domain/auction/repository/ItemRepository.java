26737b9bf479403a3c1e0cc70072ad8dabc1368f
leeinhong95 <leeinhong9512@gmail.com>
2024-04-25 14:57:06 +0900

    fix : ItemRepository 수정중

--- src/main/java/com/kcs3/panda/domain/auction/repository/ItemRepository.java
+++ src/main/java/com/kcs3/panda/domain/auction/repository/ItemRepository.java
@@ -2,6 +2,32 @@ package com.kcs3.panda.domain.auction.repository;
 
 import com.kcs3.panda.domain.auction.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+import java.util.List;
+
+public interface  ItemRepository extends JpaRepository<Item, Long> {
+
+
+    /**
+     *  친구 목록 조회
+     */
+    @Query("selectem p from AuctionProgressIt p join p.  u where f.user1.id = :userId and f.status = 1")
+    List<Long> findByItemList(@Param("userId") Long userId);
+
+    @Query("select u.userId from Friend f join f.user1 u where f.user2.id = :userId and f.status = 1")
+    List<Long> findByReceiveFriendList1(@Param("userId") Long userId);
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+    public interface AuctionRepository extends JpaRepository<AuctionItem, Long> {
+
+
+    }
+
+
 }
