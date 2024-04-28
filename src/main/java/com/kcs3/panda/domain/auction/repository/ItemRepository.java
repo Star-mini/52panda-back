3e91f706141635be47f36654a99a5553a2fc1ecb
InhongLee95 <134349722+InhongLee95@users.noreply.github.com>
2024-04-28 10:40:04 +0900

    Update ItemRepository.java

--- src/main/java/com/kcs3/panda/domain/auction/repository/ItemRepository.java
+++ src/main/java/com/kcs3/panda/domain/auction/repository/ItemRepository.java
@@ -10,14 +10,7 @@ import java.util.List;
 public interface  ItemRepository extends JpaRepository<Item, Long> {
 
 
-    /**
-     *  친구 목록 조회
-     */
-    @Query("selectem p from AuctionProgressIt p join p.  u where f.user1.id = :userId and f.status = 1")
-    List<Long> findByItemList(@Param("userId") Long userId);
 
-    @Query("select u.userId from Friend f join f.user1 u where f.user2.id = :userId and f.status = 1")
-    List<Long> findByReceiveFriendList1(@Param("userId") Long userId);
 
 
 
