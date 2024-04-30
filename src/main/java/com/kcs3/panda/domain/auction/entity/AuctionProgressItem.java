7e3e2a7c66bdb9d4d1b8b3b0a8bf965a76803f09
Sang Min Park <minisstar@naver.com>
2024-04-30 22:51:19 +0900

    fix:AuctionProgressItem main으로부터 적용

--- src/main/java/com/kcs3/panda/domain/auction/entity/AuctionProgressItem.java
+++ src/main/java/com/kcs3/panda/domain/auction/entity/AuctionProgressItem.java
@@ -5,21 +5,31 @@ import com.kcs3.panda.domain.user.entity.User;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicUpdate
+@Table(name = "AuctionProgressItem")
 public class AuctionProgressItem extends BaseEntity {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="auctionProgressItemId", nullable = false)
     private Long auctionProgressItemId;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "itemId", nullable = false)
+    private Item item;
 
     @Column(nullable = false)
     private String ItemTitle;
@@ -36,9 +46,10 @@ public class AuctionProgressItem extends BaseEntity {
 
     @Column(nullable = false)
     private String location;
-    @Column(nullable = false)
-    private int maxPrice;
-    private String maxPersonID;
 
-
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = true)
+    private User user;
+    private String maxPersonNickName;
+    private Integer maxPrice;
 }
