5f108de9011700337b32e1884d0523051ffc4f3e
Sang Min Park <minisstar@naver.com>
2024-04-30 22:50:12 +0900

    fix:AuctionCompleteItemRepository 수정

--- src/main/java/com/kcs3/panda/domain/auction/repository/AuctionCompleteItemRepository.java
+++ src/main/java/com/kcs3/panda/domain/auction/repository/AuctionCompleteItemRepository.java
@@ -8,8 +8,4 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface AuctionCompleteItemRepository extends JpaRepository<AuctionCompleteItem, Long> {
-    @Query("SELECT i.auctionCompleteItem FROM Item i WHERE i.itemId=:itemId")
-    Optional<AuctionCompleteItem> findByitemId(@Param("itemId")Long itemId);
-
-
 }
