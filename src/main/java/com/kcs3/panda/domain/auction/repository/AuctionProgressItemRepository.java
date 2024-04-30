3a26d07459859823b7011e65af127c487eb61e79
mink <frog050406@gmail.com>
2024-04-30 14:02:52 +0900

    fix:LikeItemRepository

--- src/main/java/com/kcs3/panda/domain/auction/repository/AuctionProgressItemRepository.java
+++ src/main/java/com/kcs3/panda/domain/auction/repository/AuctionProgressItemRepository.java
@@ -1,8 +1,15 @@
 package com.kcs3.panda.domain.auction.repository;
 
 import com.kcs3.panda.domain.auction.entity.AuctionProgressItem;
+import com.kcs3.panda.domain.auction.entity.Item;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AuctionProgressItemRepository extends JpaRepository<AuctionProgressItem, Long> {
+    @Query("SELECT i.auctionProgressItem FROM Item i WHERE i.itemId=:itemId")
+    Optional<AuctionProgressItem> findByitemId(@Param("itemId") Long itemId);
 
 }
