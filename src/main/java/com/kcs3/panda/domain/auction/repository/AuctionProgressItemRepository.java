289e6f25f18e10ece1a5ea7f9d25d51595a2b96d
hahyeon-cho <mondlicht3724@gmail.com>
2024-05-01 13:47:15 +0900

    feat: 현재 최고 가격에 관한 데이터 조회

--- src/main/java/com/kcs3/panda/domain/auction/repository/AuctionProgressItemRepository.java
+++ src/main/java/com/kcs3/panda/domain/auction/repository/AuctionProgressItemRepository.java
@@ -1,8 +1,12 @@
 package com.kcs3.panda.domain.auction.repository;
 
+import com.kcs3.panda.domain.auction.dto.AuctionBidHighestDto;
 import com.kcs3.panda.domain.auction.entity.AuctionProgressItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuctionProgressItemRepository extends JpaRepository<AuctionProgressItem, Long> {
+import java.util.Optional;
 
+public interface AuctionProgressItemRepository extends JpaRepository<AuctionProgressItem, Long> {
+    Optional<AuctionBidHighestDto> findAuctionBidHighestByAuctionProgressItemId(Long auctionProgressItemId);
 }
+
