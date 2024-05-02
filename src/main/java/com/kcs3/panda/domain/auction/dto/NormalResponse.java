6f3ae4606a4cee64a3e2c509692f72623dbc23b2
Sang Min Park <minisstar@naver.com>
2024-05-02 16:42:28 +0900

    feat:NormalResponse 게터,세터 추가

--- src/main/java/com/kcs3/panda/domain/auction/dto/NormalResponse.java
+++ src/main/java/com/kcs3/panda/domain/auction/dto/NormalResponse.java
@@ -3,8 +3,17 @@ package com.kcs3.panda.domain.auction.dto;
 public class NormalResponse {
     private String status;
     private String message;
-    public NormalResponse(String status, String message){
+
+    public NormalResponse(String status, String message) {
         this.status = status;
         this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
