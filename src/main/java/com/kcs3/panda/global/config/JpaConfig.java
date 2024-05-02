0f1d878c6e0dd7dd50796a8717b57cf02f29f0cf
Sang Min Park <minisstar@naver.com>
2024-05-02 16:41:59 +0900

    feat:JpaConfig 추가

---
+++ src/main/java/com/kcs3/panda/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.kcs3.panda.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
