2bf05a976eaa1bfcfa5631f70ca910c755c5fba6
Sang Min Park <minisstar@naver.com>
2024-05-02 16:40:57 +0900

    fix:카테고리 검색 레포지토리 수정

--- src/main/java/com/kcs3/panda/domain/auction/repository/CategoryRepository.java
+++ src/main/java/com/kcs3/panda/domain/auction/repository/CategoryRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    @Query("SELECT i.category FROM Item i WHERE i.itemId=:itemId")
-    Optional<Category> findByItemId(@Param("itemId") Long itemId);
-
+    @Query("SELECT c FROM Category c WHERE c.category = :category")
+    Optional<Category> findByName(@Param("category") String category);
 }
