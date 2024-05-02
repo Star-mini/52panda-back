04487808853026203248759570c98ea16e1d6615
Sang Min Park <minisstar@naver.com>
2024-05-02 16:38:49 +0900

    fix: 물건 보내기 컨트롤러 수정

--- src/main/java/com/kcs3/panda/domain/auction/controller/AuctionItemController.java
+++ src/main/java/com/kcs3/panda/domain/auction/controller/AuctionItemController.java
@@ -4,19 +4,22 @@ import com.kcs3.panda.domain.auction.dto.AuctionItemRequest;
 import com.kcs3.panda.domain.auction.dto.NormalResponse;
 import com.kcs3.panda.domain.auction.dto.QnaPostRequest;
 import com.kcs3.panda.domain.auction.dto.CommentRequest;
+import com.kcs3.panda.domain.auction.entity.Category;
+import com.kcs3.panda.domain.auction.entity.TradingMethod;
 import com.kcs3.panda.domain.auction.service.ItemService;
 import com.kcs3.panda.domain.auction.service.LikeService;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -63,13 +66,35 @@ public class AuctionItemController
         String status = "success";
         return ResponseEntity.status(HttpStatus.CREATED).body(new NormalResponse(status,message));
     }
-    @PostMapping("/form/")
-    public ResponseEntity<NormalResponse> postAuctionItem(@RequestBody AuctionItemRequest request) throws IOException {
+    @PostMapping(value = "/form/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<NormalResponse> postAuctionItem(
+            @RequestParam("title") String title,
+            @RequestParam("images") List<MultipartFile> images,
+            @RequestParam("category") Category category,
+            @RequestParam("trading_method") TradingMethod tradingMethod,
+            @RequestParam("start_price") int startPrice,
+            @RequestParam("buy_now_price") int buyNowPrice,
+            @RequestParam("contents") String contents,
+            @RequestParam("finish_time") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime finishTime
+    ) throws IOException {
+
+        AuctionItemRequest request = new AuctionItemRequest();
+        request.title = title;
+        request.images = images;
+        request.category = category;
+        request.trading_method = tradingMethod;
+        request.start_price = startPrice;
+        request.buy_now_price = buyNowPrice;
+        request.contents = contents;
+        request.finish_time = finishTime;
+
         itemService.postItem(request);
+
         String message = "물품 등록을 성공하였습니다";
         String status = "success";
-        return ResponseEntity.status(HttpStatus.CREATED).body(new NormalResponse(status,message));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new NormalResponse(status, message));
     }
+
 
     @PostMapping("/{itemid}/like/")
     public ResponseEntity<NormalResponse> postItemLike(@PathVariable("itemid") long itemid){
