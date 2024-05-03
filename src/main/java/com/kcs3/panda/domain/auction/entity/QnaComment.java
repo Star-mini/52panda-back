70fbdceaba98c719e7a1d5b58da370ed41e2227f
Sang Min Park <minisstar@naver.com>
2024-05-03 13:25:28 +0900

    fix:질문등록 엔터티 수정

--- src/main/java/com/kcs3/panda/domain/auction/entity/QnaComment.java
+++ src/main/java/com/kcs3/panda/domain/auction/entity/QnaComment.java
@@ -2,34 +2,25 @@ package com.kcs3.panda.domain.auction.entity;
 
 import com.kcs3.panda.domain.model.BaseEntity;
 import jakarta.persistence.*;
-
-import java.time.LocalDateTime;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.DynamicUpdate;
-
+import lombok.*;
 
 @EqualsAndHashCode(callSuper = true)
-@Entity
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@DynamicUpdate
 @Table(name = "QnaComment")
 public class QnaComment extends BaseEntity {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="qnaCommentId", nullable = false)
     private Long qnaCommentId;
 
-    @OneToOne(fetch = FetchType.LAZY, optional = true)
-    private ItemQuestion itemQuestion;
-    private LocalDateTime commentTime;
-    @Column(nullable=false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "questionId", nullable = false)
+    private ItemQuestion questionId;
+
+    @Column(nullable = false)
     private String comment;
 }
