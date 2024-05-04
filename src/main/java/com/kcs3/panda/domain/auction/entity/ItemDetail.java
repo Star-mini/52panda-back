d99b9237cf68f54ca8af673b649c58078ae8a2f0
Sang Min Park <minisstar@naver.com>
2024-05-04 18:07:50 +0900

    fix: 물건저장 로직을 위한 아이템 디테일 엔터티 수정

--- src/main/java/com/kcs3/panda/domain/auction/entity/ItemDetail.java
+++ src/main/java/com/kcs3/panda/domain/auction/entity/ItemDetail.java
@@ -4,6 +4,7 @@ import com.kcs3.panda.domain.model.BaseEntity;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,4 +32,16 @@ public class ItemDetail extends BaseEntity {
 
     @Column(nullable = false)
     private String itemDetailContent;
+
+    @OneToMany(mappedBy = "itemDetail", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ItemImage> images;
+
+    @Override
+    public String toString() {
+        return "ItemDetail{" +
+                "id=" + getItemDetailId() +  // BaseEntity에서 상속받은 ID
+                ", content='" + itemDetailContent + '\'' +
+                ", item=" + (item != null ? "Item[id=" + item.getItemId() + "]" : "null") +
+                '}';
+    }
 }
