f37dd8a8910da53c6302f942fbce20efd45b812e
Sang Min Park <minisstar@naver.com>
2024-05-29 17:05:22 +0900

    feat:dto 만드는 url도 수정

--- src/main/java/com/kcs3/panda/domain/auction/controller/NoAuthAuctionController.java
+++ src/main/java/com/kcs3/panda/domain/auction/controller/NoAuthAuctionController.java
@@ -71,7 +71,7 @@ public class NoAuthAuctionController {
     }
 
     // 플라스크에서 받은 아이템list dto로 작성
-    @PostMapping("/Recommendation/Embedding/makeDto")
+    @PostMapping("/Recommendation/makeDto")
     public ResponseEntity<NormalResponse> makeDtoFromEmbedding(@RequestBody List<Long> itemIds) {
         try {
             List<RecommendDto> itemDetails = itemService.getItemsByIds(itemIds);
