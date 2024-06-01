ada2467b68847c801413be1e1a27bd30e462ee4a
Sang Min Park <minisstar@naver.com>
2024-06-01 15:34:50 +0900

    feat:추천용 레포지토리생성

---
+++ src/main/java/com/kcs3/panda/domain/auction/repository/RecommendRepository.java
@@ -0,0 +1,7 @@
+package com.kcs3.panda.domain.auction.repository;
+
+import com.kcs3.panda.domain.auction.entity.Recommend;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecommendRepository extends JpaRepository<Recommend, Long> {
+}
