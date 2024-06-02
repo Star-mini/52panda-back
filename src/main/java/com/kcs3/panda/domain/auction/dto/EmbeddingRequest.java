3e3888fe5d2cce906d4cd48b8fbc336dd5c09954
Sang Min Park <minisstar@naver.com>
2024-06-02 17:28:19 +0900

    feat:추천할때 전해주는 dto 수정

--- src/main/java/com/kcs3/panda/domain/auction/dto/EmbeddingRequest.java
+++ src/main/java/com/kcs3/panda/domain/auction/dto/EmbeddingRequest.java
@@ -4,8 +4,5 @@ import lombok.Data;
 
 @Data
 public class EmbeddingRequest {
-    private double[] embedding;
-    private double[] thEmbedding;
-    private double[] categoryEmbedding;
-    private double[] detailEmbedding;
+    private long id;
 }
